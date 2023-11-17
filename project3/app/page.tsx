60ccd2feda82b4fba00bcccfa4046d4fa31864dd
Silvan Kohler <silvankohler@proton.me>
2023-11-17 18:18:26 +0100

    add nextjs projects

---
+++ project3/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Monorepo test</h1>
+      <h2 className={styles.subtitle}>Project 3</h2>
+    </main>
+  )
+}
